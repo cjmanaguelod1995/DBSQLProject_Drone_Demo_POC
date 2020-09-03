77c1e8211b0737045e6c2f66d38f458e9e112041
cjmanaguelod1995 <cjmanaguelod1995@users.noreply.github.com>
2020-09-03 14:33:32 +0800

    Add another column.

--- DBProject_Drone_Demo/scripts/Brands.sql
+++ DBProject_Drone_Demo/scripts/Brands.sql
@@ -2,5 +2,6 @@
 	brand_id INT IDENTITY (1, 1) PRIMARY KEY,
 	brand_name VARCHAR (255) NOT NULL, 
     [brand_description] VARCHAR(50) NULL, 
-    [test_column] VARCHAR(50) NULL
+    [test_column] VARCHAR(50) NULL, 
+    [anotherColumn] NVARCHAR(50) NULL
 );
