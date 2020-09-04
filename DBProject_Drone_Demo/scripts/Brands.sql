55a796b6367e91d96ad9b56e3c4d6c15cd128e1f
cjmanaguelod1995 <cjmanaguelod1995@users.noreply.github.com>
2020-09-04 15:25:57 +0800

    Modify column type. Add date_created column.

--- DBProject_Drone_Demo/scripts/Brands.sql
+++ DBProject_Drone_Demo/scripts/Brands.sql
@@ -3,5 +3,6 @@
 	brand_name VARCHAR (255) NOT NULL, 
     [brand_description] VARCHAR(50) NULL, 
     [test_column] VARCHAR(50) NULL, 
-    [anotherColumn] NVARCHAR(50) NULL
+    [anotherColumn] CHAR(10) NULL, 
+    [date_created] DATETIME NULL
 );
