1e7f5884738684574db040eacee65472cae3ceeb
cjmanaguelod1995 <cjmanaguelod1895@gmail.com>
2020-09-12 11:17:11 +0800

    Update Test.ps1

--- DBProject_Drone_Demo.build/Test.ps1
+++ DBProject_Drone_Demo.build/Test.ps1
@@ -1,23 +1,23 @@
 # Replace by your environment information
-$resourceGroupSQL = "DBProject_Drone_Demo_RG" #
-$resourceGroupStorage = "DBProject_Drone_Demo_RG"
-$sqlServer = "db-project-server-demo"
-$sqlUsername = "cjadmin"
-$sqlPassword = "Password1"
-$securePassword = ConvertTo-SecureString -String $sqlPassword -AsPlainText -Force
-$sqlCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sqlUsername, $securePassword
-$databaseName = "DBProject_Drone_Demo_POC"
-$storageAccount = "dbprojectdemodrone"
-$containerName = "sqlexport"
+$resourceGroupSQL="DBProject_Drone_Demo_RG" #
+$resourceGroupStorage="DBProject_Drone_Demo_RG"
+$sqlServer="db-project-server-demo"
+$sqlUsername="cjadmin"
+$sqlPassword="Password1"
+$securePassword= ConvertTo-SecureString -String $sqlPassword -AsPlainText -Force
+$sqlCredentials=New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sqlUsername, $securePassword
+$databaseName="DBProject_Drone_Demo_POC"
+$storageAccount="dbprojectdemodrone"
+$containerName="sqlexport"
 
 # You don't need to change the next lines
-$bacpacFilename = (Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
-$storageKeyType = "StorageAccessKey"
-$storageKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupStorage -AccountName $storageAccount)| Where-Object {$_.KeyName -eq "key2"}
-$copyDatabaseName = (Get-Date).ToString("yyyyMMddHHmmss") + "-" + $databaseName
-$bacpacFilename = (Get-Date).ToString("yyyy-MM-dd") + "-" + $copyDatabaseName + ".bacpac"
-$baseStorageUri = "https://" + $storageAccount + ".blob.core.windows.net"
-$bacpacUri = $baseStorageUri + "/" + $containerName + "/" + $bacpacFilename
+$bacpacFilename=(Get-Date).ToString("yyyy-MM-dd-HH-mm") + ".bacpac"
+$storageKeyType="StorageAccessKey"
+$storageKey=(Get-AzStorageAccountKey -ResourceGroupName $resourceGroupStorage -AccountName $storageAccount)| Where-Object {$_.KeyName -eq "key2"}
+$copyDatabaseName=(Get-Date).ToString("yyyyMMddHHmmss") + "-" + $databaseName
+$bacpacFilename=(Get-Date).ToString("yyyy-MM-dd") + "-" + $copyDatabaseName + ".bacpac"
+$baseStorageUri="https://" + $storageAccount + ".blob.core.windows.net"
+$bacpacUri=$baseStorageUri + "/" + $containerName + "/" + $bacpacFilename
 
 # Copy database
 Write-Host "Copying" $databaseName "to" $copyDatabaseName
